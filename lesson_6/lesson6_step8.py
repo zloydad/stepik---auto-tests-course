b4d33ee5bd3a1ce58362b7bef544a412fb0a0380
zloy_dad <brutomen666.94@mail.ru>
2022-06-14 23:45:05 +0300

    Добавил обработку alerta, оптимизировал код

--- lesson_6/lesson6_step8.py
+++ lesson_6/lesson6_step8.py
@@ -1,4 +1,3 @@
-#%%
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -11,15 +10,26 @@ browser.get(link)
 
 #* говорим Selenium проверять в течение 5 секунд, пока кнопка не станет кликабельной
 button = WebDriverWait(browser, 20).until(
-EC.text_to_be_present_in_element((By.ID, "price"), "100 RUR"))
-browser.find_element_by_id("book").click()
+EC.text_to_be_present_in_element((By.ID, "price"), "$100"))
+browser.find_element(by = By.ID, value = "book").click()
 
-x = browser.find_element_by_id("input_value").text
+input_text = browser.find_element(by = By.ID, value = "input_value").text
 
-input_name = browser.find_element_by_id("answer")
-input_name.send_keys(str(math.log(abs(12*math.sin(int(x))))))
+input_name = browser.find_element(by = By.ID, value = "answer")
+input_name.send_keys(str(math.log(abs(12*math.sin(int(input_text))))))
 
-solve = browser.find_element_by_css_selector("button#solve")
+solve = browser.find_element(by = By.CSS_SELECTOR, value = "button#solve")
 solve.click()
+#* Получаем ответ из alert
+alert = browser.switch_to.alert
+alertText = alert.text
+alert.accept()
 
-# %%
+browser.close()
+#* Парсим ответ и выводим
+list = alertText.split()
+x = 0
+for i in list:
+    x = x + 1
+
+print(list[x-1])
